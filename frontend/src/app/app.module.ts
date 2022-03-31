6639a76b57be53cf2ca43d8e3ea8f4a3db6d6d99
R0tenur <himself@jenshunt.se>
2022-03-31 16:04:42 +0200

    get rid of unused import

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { ReactiveFormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { MermaidViewerComponent } from './components/mermaid-viewer/mermaid-viewer.component';
