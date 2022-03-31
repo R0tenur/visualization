523091c72b7430057ae565328032c30a5974137a
R0tenur <himself@jenshunt.se>
2022-03-31 15:57:42 +0200

    clean up code smells

--- frontend/src/app/app-testing.module.ts
+++ frontend/src/app/app-testing.module.ts
@@ -1,31 +1,30 @@
 /* istanbul ignore file */
 import { NgModule } from '@angular/core';
-import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { ReactiveFormsModule } from '@angular/forms';
 import { RouterTestingModule } from '@angular/router/testing';
 
 import { ButtonComponent } from './components/button/button.component';
-import { MERMAID } from './services/mermaid.token';
 import { WINDOW } from './services/window.token';
 let list: (e: Event) => void;
 const fakeWindowProvider = ({
   provide: WINDOW, useFactory: () => ({
     innerHeight: 100,
     innerWidth: 100,
-    removeEventListener: (type: string, listener: () => void) => {
+    removeEventListener: (_: string, listener: () => void) => {
       list = undefined as any as (e: Event) => void;
     },
-    addEventListener: (type: string, listener: () => void) => {
+    addEventListener: (_: string, listener: () => void) => {
       list = listener;
     },
     dispatchEvent: (e: Event) => list(e),
     document: {
-      addEventListener: (type: string, listener: () => void) => {
+      addEventListener: (_: string, listener: () => void) => {
         list = listener;
       },
-      getElementsByTagName: (tag: string) => [
+      getElementsByTagName: (_: string) => [
         {
-          hasAttribute: (attribute: string) => false,
-          getAttribute: (attribute: string) => '',
+          hasAttribute: (__: string) => false,
+          getAttribute: (__: string) => '',
 
         }
       ]
@@ -33,12 +32,6 @@ const fakeWindowProvider = ({
   })
 });
 
-const fakeMermaidProvider = ({
-  provide: MERMAID, useFactory: () => ({
-    render: (id: string, markdown: string, callback: () => void) => jasmine.createSpy(),
-    initialize: jasmine.createSpy(),
-  })
-});
 @NgModule({
   declarations: [
     ButtonComponent,
