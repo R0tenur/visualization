cba9b69fea3fc76baf6f59175bd7159d2ddd215e
R0tenur <himself@jenshunt.se>
2022-04-01 13:44:51 +0200

    remove unused scaffolded scss

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -9,7 +9,6 @@ import { AddRelation, addRelationKey } from './models/add-relation.model';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
 
