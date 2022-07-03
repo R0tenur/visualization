6782aaf6524a7189cd83f734e29ecca58c0ade27
R0tenur <himself@jenshunt.se>
2022-07-03 12:20:59 +0200

    fix: add temp delay to fix flaky qick pick #88

--- backend/src/message.handler.ts
+++ backend/src/message.handler.ts
@@ -3,6 +3,7 @@ import { exportService } from "./services/export.service";
 
 export const messageHandler = async (e: any) => {
     const selectable = !e.data.chart ? ['md'] : ['svg', 'md'];
+    await new Promise(resolve => setTimeout(resolve, 100));
     const selected = await window.showQuickPick([...selectable]);
     let data = e.data;
     await exportService(`chart.${selected}`, data, selected);
