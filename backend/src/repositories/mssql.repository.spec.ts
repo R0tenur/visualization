6f29b90f2bebf8c94a3b53f366fd4914e3a7f747
Jens Hunt <himself@jenshunt.se>
2023-08-21 17:05:28 +0200

    test(repo): data types, nullables and constraints

--- backend/src/repositories/mssql.repository.spec.ts
+++ backend/src/repositories/mssql.repository.spec.ts
@@ -13,10 +13,64 @@ it("maps db response to tables", async () => {
   };
   spyOn(base, "runQuery").and.returnValues(
     [
-      { COLUMN_NAME: "col1", ...dbRow },
-      { COLUMN_NAME: "col2", ...dbRow },
+      {
+        COLUMN_NAME: "col1",
+        IS_NULLABLE: "YES",
+        DATA_TYPE: "VARCHAR",
+        CHARACTER_MAXIMUM_LENGTH: 12345,
+        CONSTRAINT_TYPE: "CONSTRAINT1,CONSTRAINT2",
+
+        ...dbRow,
+      },
+      {
+        COLUMN_NAME: "col2",
+        DATA_TYPE: "DATETIME",
+        IS_NULLABLE: "NO",
+        ...dbRow,
+      },
     ],
     []
+  );
+
+  // Act
+  var result = await getMssqlDbSchema("dummyId", "dummyName");
+
+  // Asser
+  expect(result.tables).toHaveLength(1);
+  expect(result.tables[0].columns).toHaveLength(2);
+
+  expect(result.tables[0].columns[0].dataType).toBe("VARCHAR(12345)");
+  expect(result.tables[0].columns[0].nullable).toBe(true);
+  expect(result.tables[0].columns[0].constraints).toEqual([
+    "CONSTRAINT1",
+    "CONSTRAINT2",
+  ]);
+
+  expect(result.tables[0].columns[1].dataType).toBe("DATETIME");
+  expect(result.tables[0].columns[1].nullable).toBe(false);
+  expect(result.tables[0].columns[1].constraints).toEqual([]);
+});
+
+it("maps db views to tables", async () => {
+  // Arrange
+  spyOn(base, "runQuery").and.returnValues(
+    [],
+    [
+      {
+        VIEW_SCHEMA: "schema",
+        VIEW_NAME: "view",
+        COLUMN_NAME: "column0",
+        TABLE_SCHEMA: "tableSchema",
+        TABLE_NAME: "tableName",
+      },
+      {
+        VIEW_SCHEMA: "schema",
+        VIEW_NAME: "view",
+        COLUMN_NAME: "column1",
+        TABLE_SCHEMA: "tableSchema",
+        TABLE_NAME: "tableName",
+      },
+    ]
   );
 
   // Act
